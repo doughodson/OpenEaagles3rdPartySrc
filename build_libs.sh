2c51f00c98a3a79bbd36361c3b5319b42175fa3a
Doug Hodson <doug@openeaagles.org>
2016-05-24 20:18:05 -0400

    updated build script

--- build_libs.sh
+++ build_libs.sh
@@ -3,96 +3,91 @@
 # initialize the location to install libraries
 init_install_location()
 {
+  # location of install libs
   rm -rf $OE_3RD_PARTY_ROOT
   mkdir $OE_3RD_PARTY_ROOT
+  # temporary location to build libs
+  rm -rf tmp
+  mkdir tmp
 }
 
 # cigi
 install_cigi()
 {
-  rm -rf ccl
-  tar xzvf ccl_3_3_3a.tar.gz
-  cd ccl
+  tar xzvf ccl_3_3_3a.tar.gz --directory tmp
+  cd tmp/ccl
   ./configure --prefix=$OE_3RD_PARTY_ROOT --disable-shared
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # jsbsim
 install_jsbsim()
 {
-  rm -rf jsbsim
-  tar xzvf jsbsim_cvs_v2015_0704.tgz
-  cd jsbsim
+  tar xzvf jsbsim_cvs_v2015_0704.tgz --directory tmp
+  cd tmp/jsbsim
   ./autogen.sh --prefix=$OE_3RD_PARTY_ROOT --enable-libraries
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # protobuf
 install_protobuf()
 {
-  rm -rf protobuf-2.6.1
-  tar xzvf protobuf-2.6.1.tar.gz
-  cd protobuf-2.6.1
+  tar xzvf protobuf-2.6.1.tar.gz --directory tmp
+  cd tmp/protobuf-2.6.1
   ./configure --prefix=$OE_3RD_PARTY_ROOT --disable-shared
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # zeromq
 install_zeromq()
 {
-  cd zeromq
-  rm -rf zeromq-4.0.3
-  tar xzvf zeromq-4.0.3.tar.gz
-  cd zeromq-4.0.3
+  tar xzvf zeromq/zeromq-4.0.3.tar.gz --directory tmp
+  cd tmp/zeromq-4.0.3
   ./configure --prefix=$OE_3RD_PARTY_ROOT
   make
   make install
-  cd ..
-  cp zmq.hpp $OE_3RD_PARTY_ROOT/include
-  cd ..
+  cd ../..
+  cp zeromq/zmq.hpp $OE_3RD_PARTY_ROOT/include
 }
 
 # FOX GUI
 install_fox_v1_7()
 {
-  rm -rf fox-1.7.55
-  tar xzvf fox-1.7.55.tar.gz
-  cd fox-1.7.55
+  tar xzvf fox-1.7.55.tar.gz --directory tmp
+  cd tmp/fox-1.7.55
   ./configure --prefix=$OE_3RD_PARTY_ROOT
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # FLTK GUI
 install_fltk_v1_3()
 {
-  rm -rf fltk-1.3.3
-  tar xzvf fltk-1.3.3-source.tar.gz
-  cd fltk-1.3.3
+  tar xzvf fltk-1.3.3-source.tar.gz --directory tmp
+  cd tmp/fltk-1.3.3
   ./configure --prefix=$OE_3RD_PARTY_ROOT
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # wxWidgets
 install_wxWidgets()
 {
-  rm -rf wxWidgets-3.1.0
   bzip2 --keep --decompress wxWidgets-3.1.0.tar.bz2
-  tar xvf wxWidgets-3.1.0.tar
-  cd wxWidgets-3.1.0
+  tar xvf wxWidgets-3.1.0.tar --directory tmp
+  cd tmp/wxWidgets-3.1.0
   ./configure --prefix=$OE_3RD_PARTY_ROOT --disable-shared
   make
   make install
-  cd ..
+  cd ../..
 }
 
 # check for location to install 3rd party libs
